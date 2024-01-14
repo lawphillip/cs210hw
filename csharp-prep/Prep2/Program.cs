2ae4275fa65272e4ead0d438103cb264c30d6db9
Phillip Law <law17015@byui.edu>
2024-01-13 23:50:50 -0700

    complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,33 +5,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("nme");
+        //Console.WriteLine("nme");
 
         // creating variables 
-        int mycount = 34;
-        string name = "Phil";
-        var green = "green";
-        var myothercount = 3;
+        //int mycount = 34;
+        //string name = "Phil";
+        //var green = "green";
+        //var myothercount = 3;
         // writing 
-        Console.Write($"a name: {green}");
-        Console.WriteLine(name);
+        //Console.Write($"a name: {green}");
+        //Console.WriteLine(name);
         // read line user input 
-        System.Console.WriteLine("what is your age");
-        var agestring = Console.ReadLine();
-        // converting variables 
-        var age = int.Parse(agestring);
-        // conditional
-        if (age < 18)
-        {
-            System.Console.WriteLine("you're a minor");
-        }
-        else
-        {
-            System.Console.WriteLine("you're old");
-        }
+        System.Console.WriteLine("what is your grade? ");
+        var gradestring = Console.ReadLine();
         
-
-
-
+        // converting variables 
+        var grade = int.Parse(gradestring);
+        // conditional
+        var letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("You Passed");
+        }
+        Console.WriteLine($"Your grade is {letter}");
     }
 }
