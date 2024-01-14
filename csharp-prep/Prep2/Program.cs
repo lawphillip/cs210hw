a4e86b9e8b6e19a0aae5506712e8f1315e7df975
Phillip Law <law17015@byui.edu>
2024-01-13 23:03:34 -0700

    added examples of code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("nme");
+
+        // creating variables 
+        int mycount = 34;
+        string name = "Phil";
+        var green = "green";
+        var myothercount = 3;
+        // writing 
+        Console.Write($"a name: {green}");
+        Console.WriteLine(name);
+        // read line user input 
+        System.Console.WriteLine("what is your age");
+        var agestring = Console.ReadLine();
+        // converting variables 
+        var age = int.Parse(agestring);
+        // conditional
+        if (age < 18)
+        {
+            System.Console.WriteLine("you're a minor");
+        }
+        else
+        {
+            System.Console.WriteLine("you're old");
+        }
+        
+
+
+
     }
 }
