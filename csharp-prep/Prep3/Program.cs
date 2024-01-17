c8be80bd66859e7fb428ca010011aa1f89626d14
Phillip Law <law17015@byui.edu>
2024-01-17 10:00:45 -0700

    adding code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Net.Http.Headers;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // while loop 
+        int count = 5;
+        while (count < 10)
+        {
+        Console.WriteLine($"count = {count}");
+        count += 1;
+        }
+        // do while 
+        var anotherc = 2;
+        do {
+            Console.WriteLine($"another count = {anotherc++}");
+        } while(anotherc < 10);
+
+        //for loop 
+        for (var i =0; i < 5; i++)
+        {
+            Console.WriteLine($"i = {i}");
+        }
+        // list 
+        List<int> myints = new List<int>();
+        var moreInt = new List<int>();
+        moreInt.Add(34);
+        moreInt.Add(23);
+
+        int guess = int.Parse(Console.ReadLine());
     }
 }
