b558b26e0a04c1393ea8b8d9dc29bb266749decf
Phillip Law <law17015@byui.edu>
2024-01-19 19:57:47 -0700

    adding list prep

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Text.RegularExpressions;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // list 
+        List<int> myints = new List<int>();
+        var moreInt = new List<int>();
+        //addingitems
+        moreInt.Add(34);
+        moreInt.Add(23);
+
+        //foreach
+        foreach(var n in moreInt)
+        {
+            Console.WriteLine($"N = {n}");
+        }
+
+        List<int> numberline = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        var number = 10000;
+        do
+        {
+            Console.Write("Enter number ");
+            number = int.Parse(Console.ReadLine());
+            numberline.Add(number);
+        }while (number != 0);
+        double total = 0;
+        foreach (int num in numberline)
+        {
+            total += num;
+        }
+        foreach(var n in numberline)
+        {
+            Console.WriteLine($"N = {n}");
+        }
+        Console.WriteLine($"the sum is: {total}");
+        double average = total / (numberline.Count -1);
+        Console.WriteLine($"The average is {average}");
+        int max = numberline.Max();
+        Console.WriteLine($"The Largest number is {max}");
     }
 }
