3d2085e1dbc35afbf8b7759db652648c05d70126
lawphillip <156010551+lawphillip@users.noreply.github.com>
2024-02-17 23:38:19 -0700

    Create Reference.cs

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+class Reference
+{
+   public string Displayrefference(string reference)
+    {
+        return reference;
+    }
+}
+// will be finished by monday the 19th
