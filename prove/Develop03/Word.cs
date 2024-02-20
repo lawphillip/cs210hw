e80f6e9a5c0a86a1322534d910408f98998d18e9
lawphillip <156010551+lawphillip@users.noreply.github.com>
2024-02-20 01:01:49 -0700

    Update Word.cs

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -8,8 +8,11 @@ class Word
     {
         if(show == false)
         {
-            word = word.Replace("a", "_").Replace("b", "_" ).Replace("c", "_" ).Replace("d", "_" ).Replace("e", "_" ).Replace("f", "_" ).Replace("g", "_" ).Replace("h", "_" ).Replace("i", "_" ).Replace("j", "_" ).Replace("k", "_" ).Replace("l", "_" ).Replace("m", "_" ).Replace("n", "_" ).Replace("o", "_" ).Replace("p", "_" ).Replace("q", "_" ).Replace("r", "_" ).Replace("s", "_" ).Replace("t", "_" ).Replace("u", "_" ).Replace("v", "_" ).Replace("w", "_" ).Replace("x", "_" ).Replace("y", "_" ).Replace("z", "_" ).Replace("A", "_" ).Replace("B", "_" ).Replace("C", "_" ).Replace("D", "_" ).Replace("E", "_" ).Replace("F", "_" ).Replace("G", "_" ).Replace("H", "_" ).Replace("I", "_" ).Replace("J", "_" ).Replace("K", "_" ).Replace("L", "_" ).Replace("M", "_" ).Replace("N", "_" ).Replace("O", "_" ).Replace("P", "_" ).Replace("Q", "_" ).Replace("R", "_" ).Replace("S", "_" ).Replace("T", "_" ).Replace("U", "_" ).Replace("V", "_" ).Replace("W", "_" ).Replace("X", "_" ).Replace("Y", "_" ).Replace("Z", "_" );
+            //word.Replace('a', '_').Replace('b', '_' ).Replace('c', '_' ).Replace('d', '_' ).Replace('e', '_' ).Replace('f', '_' ).Replace('g', '_' ).Replace('h', '_' ).Replace('i', '_' ).Replace('j', '_' ).Replace('k', '_' ).Replace('l', '_' ).Replace('m', '_' ).Replace('n', '_' ).Replace('o', '_' ).Replace('p', '_' ).Replace('q', '_' ).Replace('r', '_' ).Replace('s', '_' ).Replace('t', '_' ).Replace('u', '_' ).Replace('v', '_' ).Replace('w', '_' ).Replace('x', '_' ).Replace('y', '_' ).Replace('z', '_' ).Replace('A', '_' ).Replace('B', '_' ).Replace('C', '_' ).Replace('D', '_' ).Replace('E', '_' ).Replace('F', '_' ).Replace('G', '_' ).Replace('H', '_' ).Replace('I', '_' ).Replace('J', '_' ).Replace('K', '_' ).Replace('L', '_' ).Replace('M', '_' ).Replace('N', '_' ).Replace('O', '_' ).Replace('P', '_' ).Replace('Q', '_' ).Replace('R', '_' ).Replace('S', '_' ).Replace('T', '_' ).Replace('U', '_' ).Replace('V', '_' ).Replace('W', '_' ).Replace('X', '_' ).Replace('Y', '_' ).Replace('Z', '_' );
+            String newword = new String('_', word.Length);
+            word = newword;
         }
+        
     }
     public Word(string word)
     {
@@ -22,4 +25,3 @@ class Word
         return word;
     }
 }
-// will be finished by monday the 19th
