0ef2932a1610c9698f0576b18863cf7ca8cd540e
lawphillip <156010551+lawphillip@users.noreply.github.com>
2024-02-26 11:39:06 -0700

    Update Program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,10 +1,32 @@
 using System;
-
+// moroni 10 4-5
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string thereff = "Moroni 10:3-4";
+        Scripture scripture = new Scripture();
+        var counter = 0;
+        scripture.AddWords();
+        var theref = new Reference(thereff);
+        scripture.DisplayScripture(theref);
+        while(true)
+        {
+            
+            var userinput = "";
+            Console.WriteLine("Press enter to hide words or exit to exit program");
+            userinput = Console.ReadLine();
+            Console.Clear();
+            scripture.Hidewords();
+            scripture.Hidewords();
+
+            scripture.DisplayScripture(theref);
+            if (counter == 61)
+            break;
+            else if (userinput == "exit")
+            break;
+            counter ++;
+
+        }
     }
 }
-// will be finished by monday the 19th
