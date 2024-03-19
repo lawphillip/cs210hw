f86e6237828774de485ae5cdfa4eb8745f9576a4
lawphillip <156010551+lawphillip@users.noreply.github.com>
2024-03-18 19:26:31 -0600

    Update Checklist.cs

--- prove/Develop05/Checklist.cs
+++ prove/Develop05/Checklist.cs
@@ -16,9 +16,9 @@ class Checklist : Goal
     public override void Display()
     {
         if(iscomplete == true)
-        Console.WriteLine($"[X] {Goalname} {description} -- Currently Completed {count}/{Goalcount}");
+        Console.WriteLine($"[X] {Goalname} ({description}) -- Currently Completed {count}/{Goalcount}");
         else
-        Console.WriteLine($"[ ] {Goalname} {description} -- Currently Completed {count}/{Goalcount}");
+        Console.WriteLine($"[ ] {Goalname} ({description}) -- Currently Completed {count}/{Goalcount}");
     }
 
     public override int Awardpoint()
@@ -27,18 +27,31 @@ class Checklist : Goal
         if ( count == Goalcount)
         {
             iscomplete = true;
+            Console.WriteLine($"Congradulations! You have earned {completionreward + reward} Points");
             return completionreward + reward; 
         }
         else
-            return reward;   
+        {
+            Console.WriteLine($"Congradulations! You have earned {reward} Points");
+            return reward;
+        }   
     }
-
+    public Checklist(string values) : base(values.Split($"&&")[0])
+    {
+        var checklistValues = values.Split($"{SEP}{SEP}")[1].Split(SEP);
+        iscomplete = bool.Parse(checklistValues[0]);
+        completionreward = int.Parse(checklistValues[1]);
+        Goalcount = int.Parse(checklistValues[2]);
+        count = int.Parse(checklistValues[3]);
+    }
     public override string export()
     {
-        return base.export() + $"{SEP}{iscomplete}{SEP}{completionreward}{SEP}{Goalcount}{SEP}{count}";
+        return base.export() + $"{SEP}{SEP}{iscomplete}{SEP}{completionreward}{SEP}{Goalcount}{SEP}{count}";
     }
 
-    
+
 
 }
-//i will have this code finished by 3/19/2024
+    
+
+
