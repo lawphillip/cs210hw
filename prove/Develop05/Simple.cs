00d1e8c19754e62576bf43f1ec6b2deca15c8de1
lawphillip <156010551+lawphillip@users.noreply.github.com>
2024-03-18 19:29:48 -0600

    Update Simple.cs

--- prove/Develop05/Simple.cs
+++ prove/Develop05/Simple.cs
@@ -9,20 +9,26 @@ class Simple : Goal
     public override void Display()
     {
         if (iscomplete == true)
-        Console.WriteLine($"[X] {Goalname} {description}");
+        Console.WriteLine($"[X] {Goalname} ({description})");
         else
         base.Display();
     }
     public override int Awardpoint()
     {
         iscomplete = true;
+        Console.WriteLine($"Congradulations! You have earned {reward} Points");
         return reward;
     }
     public override string export()
     {
-        return base.export() + $"{SEP}{iscomplete}";
+        return base.export() + $"{SEP}{SEP}{iscomplete}";
+    }
+
+    public Simple(string values) : base(values.Split($"&&")[0])
+    {
+        var simpleValues = values.Split($"{SEP}{SEP}")[1].Split(SEP);
+        iscomplete = bool.Parse(simpleValues[0]);
     }
 
 
 }
-//i will have this code finished by 3/19/2024
