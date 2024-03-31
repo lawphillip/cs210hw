33852bce71e1ddc08da39d9b8dcf18b485c14ef3
lawphillip <156010551+lawphillip@users.noreply.github.com>
2024-03-31 13:56:11 -0600

    Update Program.cs

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videos = new List<Video>();
+        videos.Add(new Video("Train crushes Lambo","Mr.Beast", 180));
+        videos[0].makecomment("joe", "this video is so super awesome");
+        videos[0].makecomment("momo124", "they should've given that lambo to me ");
+        videos[0].makecomment("bob", "I loved it when the train destroyed the car");
+        videos.Add(new Video("How to change the oil on a 2004 camry","timcars5", 80));
+        videos[1].makecomment("howy","what happens if the oil is brown");
+        videos[1].makecomment("ben","is it the same for the 2005");
+        videos[1].makecomment("kim","very useful saved me over 50 dollars");
+        videos.Add(new Video("Reaction to the dune 3 trailer","movies33", 120));
+        videos[2].makecomment("bigmo76","im so excited for this movie");
+        videos[2].makecomment("xxbilxx","Im so excited for timothy to kick butt");
+        videos[2].makecomment("nemos","The last movie was amazing");
+        foreach(Video video in videos)
+        {
+            video.displayvideo();
+        }
+
     }
 }
