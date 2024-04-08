c4de5fbf5e3f45de6eaa6fc9af9417e796f7a0da
lawphillip <156010551+lawphillip@users.noreply.github.com>
2024-04-08 09:26:04 -0600

    Update Program.cs

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+        activities.Add(new Cycling("April 3rd 2024", 30, 15));
+        activities.Add(new Running("April 3rd 2024", 60, 5));
+        activities.Add(new Swimming("April 3rd 2024", 60, 90));
+        foreach(Activity activity in activities)
+        {
+            Console.WriteLine(activity.getsummary());
+        }
     }
 }
